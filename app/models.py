88c67a768c16c5a18d2db4f958e58baa887c92ed
felixyin <ybkk1027@gmail.com>
2018-08-16 23:22:04 +0800

    追溯信息骨架

--- app/models.py
+++ app/models.py
@@ -72,6 +72,7 @@ class Batch(BaseModel):
     count = m.IntegerField('生成数量', blank=False)
     is_gen = m.BooleanField('已经生成？', blank=False, editable=False, default=False)
 
+    element = m.CharField('成分', max_length=50, default=None, null=True)
     agent_com = m.CharField('进口代理商', max_length=50, default=None, null=True)
     promise_book = m.ImageField('企业承诺书', upload_to='upload/batch/promise/%y/%m/', default=None, null=True)
     check_date = m.DateTimeField('检测日期', default=None, null=True)
