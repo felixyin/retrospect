d1df47fdccd1b31e780be748e317470ecce96fad
felixyin <ybkk1027@gmail.com>
2018-08-21 12:26:29 +0800

    优化图片脚本

--- update.sh
+++ update.sh
@@ -5,25 +5,37 @@ mkdir /root/backup
 
 send=`date '+%Y%m%d-%H%M%S'`
 
+
 # 压缩media目录下用户上传的图片到备份目录，并按日期命名
 tar -czvf  /root/backup/media-${send}.tar.gz /root/retrospect/media/
 
+
 # 切换到项目根目录
 cd /root/retrospect
+
 
 # 覆盖本地的方式，检出代码
 git fetch --all
 git reset --hard origin/master
 git pull
 
+
 # 赋予updat命令权限（如果命令更新了，则权限会丢失）
 sudo chmod 777 update.sh
+sudo chmod 777 zipimg.sh
+
+
+# 压缩图片
+zipimg
+
 
 # 收集静态文件，为nginx使用
 python manage.py collectstatic
 
+
 # 计算和生成数据库升级脚本
 python manage.py makemigrations
+
 
 # 执行数据库升级操作
 python manage.py migrate
--- zipimg.sh
+++ zipimg.sh
@@ -3,36 +3,48 @@
 
 cd /root/retrospect/media/
 
-# 查看一下大于200k的图片
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k
+# 查看一下大于150k的图片
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k -exec convert -resize 90%x90% {} {} \;
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
 
-find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +200k
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
+
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
+
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
+
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
+
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
+
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k -exec convert -resize 90%x90% {} {} \;
+
+
+find /root/retrospect/media/ -regex '.*\(jpg\|JPG\|png\|jpeg\)' -size +150k
 
 cd ~
 
